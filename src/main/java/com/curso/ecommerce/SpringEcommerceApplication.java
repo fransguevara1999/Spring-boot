cab4025d4d572cbe89426ecaafedc148f0977271
titutar26xd <helder26xd@gmail.com>
2022-02-08 20:17:28 -0500

    Creacion de estructura de proyecto spring-boot

---
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -0,0 +1,16 @@
+package com.curso.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//Se excluye del proyecto DataSourceAutoConfiguration para poder ejecutar y 
+//comprobar si nuestra aplicacion corre desde el comienzo. Considerando si colocamos 
+//como dependencia la conexion a la base de datos.
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApplication.class, args);
+	}
+
+}
