9147f845cf5254d588fb8dc52b95625ede5981a5
titutar26xd <helder26xd@gmail.com>
2022-02-13 16:01:14 -0500

    Se habilito el boton ver producto con descripcion y precio

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.curso.ecommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
@@ -14,6 +18,8 @@ import com.curso.ecommerce.service.ProductoServices;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductoServices productoService;
 	
@@ -21,6 +27,17 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("/productohome/{id}")
+	public String productoHome(@PathVariable Integer id,Model model) {
+		Producto producto=new Producto();
+		Optional<Producto> optionalProducto=productoService.get(id);
+		producto=optionalProducto.get();
+		log.info("Id Producto enviado como parametro {}",id);
+		model.addAttribute("producto", producto);
+		
+		return "usuario/productohome";
 	}
 
 	
